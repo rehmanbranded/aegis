f0ca2fab2d997edcb0941e96ef5e631d1f888cb1
Aravindh P <exigent@Aravindhs-MacBook-Pro.local>
2026-01-22 11:37:58 +0530

    chore: initial monorepo scaffolding

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ],
+  env: {
+    node: true,
+    es2020: true
+  },
+  ignorePatterns: ["dist/", "node_modules/"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+    "@typescript-eslint/explicit-module-boundary-types": "off"
+  }
+};
